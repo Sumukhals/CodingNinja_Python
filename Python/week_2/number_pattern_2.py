bbb8e4c70193f8564d2ecd7cb6adc90b964d4011
Sumukhals <sumukhals@gmail.com>
2023-08-20 20:33:32 +0530

    Update number_pattern_2.py

--- Python/week_2/number_pattern_2.py
+++ Python/week_2/number_pattern_2.py
@@ -5,13 +5,23 @@ from os import *
 
 ## Read input as specified in the question.
 ## Print output as specified in the question.
+## Read input as specified in the question.
+## Print output as specified in the question.
 n = int(input())
 
-i = n
+i = 1
+numToFill = 0
 
-while i > 0:
+while i <= n:
     j = 1
+    startEndNum = j + (i - 2)
+    if(startEndNum <= 0):
+        startEndNum = 1
     while j <= i:
-        print(j, end="")
+        if(j == 1 or j == i):
+            print(startEndNum, end="")
+        else:
+            print(numToFill, end="")
         j = j + 1
     print()
+    i = i + 1
